5d5ff249cd91cec793286919af42da056365c7a8
Andre <44403658+andrelv@users.noreply.github.com>
2018-11-06 19:09:49 +0000

    Add files via upload
    Aula 6/11

--- Aula 2_11/Books.js
+++ Aula 2_11/Books.js
@@ -1,56 +1,66 @@
 $(document).ready(function () {
     console.log("Ready for Google Books!");
+    $("#card").hide();
+    //$("#cabecatabela").hide();
+    //$("#tabela").hide();
+    //$("#pyro").hide();
 
 });
 
 //classes
 class Livro {
-    constructor(id, img, titulo, autor, tipo, desc) {
+    constructor(id, img, titulo, autor, tipo, desc, like, dislike) {
         this.img = img;
         this.titulo = titulo;
         this.desc = desc;
         this.autor = autor;
         this.id = id;
         this.tipo = tipo;
+        this.like = like;
+        this.dislike = dislike;
     }
 }
 
 //vars
 var listaLivros = [];
+var listaLikes = [];
+var contLikes = 0;
+var cont = 0;
+var letra1 = "";
+//var element = 0;
 
 
 $("#pedelivros").click(getMeo);
 $("#like").click(like);
 $("#dismiss").click(dismiss);
+$("#likes").click(mostraLivros);
 
 
 
 function getMeo() {
-
+    $("#cabecatabela").hide();
+    $("#card").show();
+    var nr = $('#ipt1').val();
+    letra();
     $.ajax({
-        url: "https://www.googleapis.com/books/v1/volumes?q=search+terms",
+        url: "https://www.googleapis.com/books/v1/volumes?q=" + letra1 + "&maxResults=" + nr + "&startIndex=" + (listaLivros.length + 1),
         type: 'GET',
-        //contentType: 'application/json',
         success: function (data) {
             console.log(data);
             for (var i = 0; i < data.items.length; i++) {
                 var element = data.items[i];
 
-                //console.log(element);
                 if (element.volumeInfo.imageLinks == undefined) {
                     element.volumeInfo.imageLinks = "noimage.jpeg";
                 }
-                let book = new Livro(element.id, element.volumeInfo.imageLinks.thumbnail, element.volumeInfo.title, element.volumeInfo.authors,'categoria', element.volumeInfo.description);
-               console.log(book)
+                if (element.volumeInfo.description == undefined) {
+                    element.volumeInfo.description = "Livro sem descrição para apresentar";
+                }
+                let book = new Livro(element.id, element.volumeInfo.imageLinks.thumbnail, element.volumeInfo.title, element.volumeInfo.authors, 'categoria', element.volumeInfo.description, 0, 0);
+                console.log(book)
                 listaLivros.push(book);
             }
-
-            $("#imglivro").attr("src",listaLivros[0].img);
-            $("#titulolivro").html(listaLivros[0].titulo);
-            $("#descricao").html("Descrição: " + listaLivros[0].desc);
-            $("#autor").html("Escrito por: " + listaLivros[0].autor);
-
-
+            mostrar();
         }
 
     });
@@ -58,6 +68,79 @@ function getMeo() {
 
 }
 
-function like() {
+function letra() {
+
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 2; i++)
+        letra1 = possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return letra1;
 
 }
+
+function mostrar() {
+    
+    if (listaLivros.length > 0) {
+        $("#card").show();
+    }
+    if (cont == listaLivros.length) {
+        $("#card").hide();
+        alert("A lista de livros terminou, solicite mais ou veja quais os Livros com Like");
+    }
+    else {
+        $("#imglivro").attr("src", listaLivros[cont].img);
+        $("#titulolivro").html(listaLivros[cont].titulo);
+        $("#descricao").html("Descrição: " + listaLivros[cont].desc);
+        $("#autor").html("Escrito por: " + listaLivros[cont].autor);
+    }
+}
+
+function like() {
+                                                                                                                                                              
+    ebook = listaLivros[cont];
+    ebook.like++;
+    contLikes++; //para mostrar o nr de likes dados
+    tabela();
+    $("#likes").html("Livros com Likes: " +contLikes);
+    mostrar();
+}
+
+function dismiss() {
+    ebook = listaLivros[cont];
+    ebook.dislike++;
+    cont++;
+    mostrar();
+}
+
+function tabela(){
+    //var id = listaLivros[cont].id;
+    var titulo=listaLivros[cont].titulo;
+    var autor=listaLivros[cont].autor;
+    var lk = listaLivros[cont].like;
+    listaLikes.push(titulo, autor);
+    for (i=0; i > contLikes; i++){
+        if (listaLikes[contLikes] == listaLikes[i]){
+            lk++;
+        }
+    }
+    $("#tabela").append(getRow(titulo, autor, lk));
+    cont++;
+}
+
+function mostraLivros() {
+    console.log('entou');
+    $("#cabecatabela").css("display", "block");
+   // $("#cabecatabela").show();
+    //$("#tabela").show();
+
+}
+
+function getRow(titulo, autor, lk) {
+    
+        return `<tr>
+        <td>${titulo}</td>
+        <td>${autor}</td>
+        <td>${lk}</td>
+        </tr>`;
+    }
